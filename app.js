e9420ef479ebd415f4fc4b80394238ae2c32717a
natalietate <martin.nat@gmail.com>
2018-01-16 11:05:06 -0500

    update port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const routes = require('./router');
 
 // Define the hostname and port where the server can be found
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Define the directory where static files (images, html, css) are found
 // saved in /public folder
